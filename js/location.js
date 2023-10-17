0af6b504abb63e20b1e75a2a7d27da82b4bf99ff
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-17 15:26:49 +0100

    updated

---
+++ js/location.js
@@ -0,0 +1,18 @@
+export function getUserLocation() {
+	return new Promise((resolve, reject) => {
+		if (!navigator.geolocation) {
+			console.log("Geolocation is not supported by this browser.");
+			return;
+		}
+
+		navigator.geolocation.getCurrentPosition(
+			({ coords }) => {
+				resolve(coords);
+			},
+			(error) => {
+				reject(error);
+				console.log("Unable to fetch location: " + error.message);
+			}
+		);
+	});
+}
