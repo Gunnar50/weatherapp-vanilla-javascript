8341acd37c37dd78978299a3117adf5286e062df
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-18 22:15:14 +0100

    added user input

--- js/findUserController.js
+++ js/findUserController.js
@@ -5,7 +5,7 @@ export const findUser = () => {
 	const findMe = document.querySelector("[data-findme]");
 
 	findMe.addEventListener("click", async () => {
-		console.log("find me");
+		// console.log("find me");
 		const { latitude, longitude } = await getUserLocation();
 		getWeather(latitude, longitude);
 	});
