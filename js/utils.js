f635696648f5aef1899354c322498e9fd5b10852
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-21 22:27:18 +0100

    added comments

--- js/utils.js
+++ js/utils.js
@@ -5,6 +5,7 @@ import {
 	todayContainerRef,
 } from "./config.js";
 
+// format time into hh:mm am/pm
 export const formatTime = (time) => {
 	let date;
 	if (typeof time === "string") {
@@ -23,6 +24,7 @@ export const formatTime = (time) => {
 	return `${hours}:${String(minutes).padStart(2, "0")}${ampm}`;
 };
 
+// formate a date into weekDay, day month.
 export const getFormattedDate = (dateString) => {
 	const days = [
 		"Sunday",
@@ -56,14 +58,15 @@ export const getFormattedDate = (dateString) => {
 	return `${dayName}, ${day} ${month}`;
 };
 
+// returns the current time formatted
 export const updateCurrentTime = () => {
 	return formatTime(Date.now() / 1000);
 };
 
+// takes the wind in degrees and returns the cardinal direction
 export const getWindDirection = (degrees) => {
 	let direction = "";
 
-	// Determine the cardinal direction
 	if (degrees >= 330 || degrees <= 30) {
 		direction = "N";
 	} else if (degrees > 30 && degrees <= 60) {
@@ -87,6 +90,7 @@ export const getWindDirection = (degrees) => {
 	return direction;
 };
 
+// check the screen size and change the order of some elements in the DOM
 export const checkScreenSize = () => {
 	if (window.innerWidth >= mediumScreen) {
 		todayContainerRef.insertBefore(
@@ -96,4 +100,76 @@ export const checkScreenSize = () => {
 	} else {
 		rootContainerRef.insertBefore(searchBarRef, todayContainerRef);
 	}
+};
+
+// returns true if the current time is day time
+// false if is night time
+export const checkDayTime = (sunset, sunrise) => {
+	if (Date.now() > sunset * 1000 || Date.now() < sunrise * 1000) {
+		return false;
+	}
+	return true;
+};
+
+// it takes the weather data and returns a number of classes depending on the icon of the data.
+// it is used for the custom cloud icons
+export const getIconsClass = (dataWeather) => {
+	const icon = dataWeather.icon;
+	const main = dataWeather.main;
+
+	const dayNight = icon.indexOf("d") !== -1 ? "day" : "night";
+
+	let upperclass = `card-${dayNight} card-`;
+	let lowerclass;
+
+	switch (icon.substring(0, 2)) {
+		case "02":
+			upperclass += "clouds";
+			lowerclass = "few-clouds-" + dayNight;
+			break;
+		case "03":
+			upperclass += "clouds";
+			lowerclass = "clouds";
+			break;
+		case "04":
+			upperclass += "more-clouds";
+			lowerclass = "more-clouds";
+			break;
+		case "01":
+			upperclass += "clear";
+			lowerclass = "clear-" + dayNight;
+			break;
+		case "13":
+			upperclass += "snow";
+			lowerclass = "snow";
+			break;
+		case "09":
+			if (main === "Drizzle") {
+				upperclass += "drizzle";
+				lowerclass = "drizzle";
+			} else {
+				upperclass += "rain";
+				lowerclass = "rain";
+			}
+			break;
+		case "10":
+			upperclass += "light-rain";
+			lowerclass = "light-rain-" + dayNight;
+			break;
+		case "11":
+			upperclass += "storm";
+			lowerclass = "storm";
+			break;
+
+		default:
+			console.log("Unknown icon type.");
+			break;
+	}
+
+	// these lines are for testing purposes
+	// console.log(upperclass, lowerclass);
+	// let card = "storm";
+	// return ["card-day card-" + card, card];
+
+	return [upperclass, lowerclass];
 };
