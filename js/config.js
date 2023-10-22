df06fdf3506d0b962364848313748a979cee3950
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-22 18:46:40 +0100

    added error messages

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 export const API_KEY = "085590031a30567d9813303060a2063c";
+
 export const API_URL = "https://api.openweathermap.org";
 
 export const [autoLat, autoLong] = [51.50722, -0.1275]; // London coords
@@ -6,7 +7,9 @@ export const [autoLat, autoLong] = [51.50722, -0.1275]; // London coords
 export const mediumScreen = 760;
 export const searchBarRef = document.querySelector("[data-search-bar]");
 
+export const bodyRef = document.getElementsByTagName("body")[0];
 export const rootContainerRef = document.getElementById("root");
 export const todayContainerRef = document.querySelector("[data-today]");
 export const weekContainerRef = document.querySelector("[data-week]");
 export const cityInput = document.querySelector("[data-location]");
+export const errorMessages = document.querySelector("[data-error]");
